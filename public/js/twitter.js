a6a22a23d7cbf4cea502b6665327c1ee6edf9f8c
Tianyou Hu <thu685@UoA.auckland.ac.nz>
2016-08-25 14:58:40 +1200

    update files

---
+++ public/js/twitter.js
@@ -0,0 +1,28 @@
+var Twit = require('twit');
+
+function getTweet(){
+console.log('The Bot is Running!');
+
+var T = new Twit({
+    consumer_key: 'AayOBhgG6Jy63toIE0Eno19YS',
+    consumer_secret: 'exgf3RbS7p7Spba3vFQx4rlfWHHDrEfa7s6iZrTbcS6ADbvheJ',
+    access_token: '826739371-3mKMZLEtUFOa2bgSHRZljbx2AkIcs9M2XjorDayK',
+    access_token_secret: 'a6N6izccToYdI5mr58WOVy7FtSXkXOC33PabXVJQpfJAa',
+    timeout_ms: 60 * 100,
+});
+// parameters to be passed into the get request
+var params = {
+    q: '#AAPL',
+    count: 10,
+    lang: 'en'
+};
+// Callback function for the get request
+function getData(err, data, response) {
+    var tweets = data.statuses;
+    for (var i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].text);
+        //documents.write(tweets[i].text);
+    }
+}
+T.get('search/tweets', params, getData);
+}
