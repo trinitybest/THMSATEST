5780fcb1158197e0c6b65e775e1f3f068f134a7f
T H <hutianyou@live.cn>
2016-08-27 12:04:10 +1200

    update

--- app.js
+++ app.js
@@ -34,13 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 //app.use('/getTweet', getTweet);
-//**************************************
-app.post('/LEDon', function(req, res) {
-    console.log('LEDon button pressed!');
-    // Run your LED toggling code here
-});
 
-app.listen(1337);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
