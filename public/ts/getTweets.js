fef6dc81c45128f2a1bb5e87573d8330b3df9f2b
T H <hutianyou@live.cn>
2016-08-27 12:10:34 +1200

    update

--- public/ts/getTweets.js
+++ public/ts/getTweets.js
@@ -2,13 +2,6 @@ $('#ledon-button').click(function () {
     alert("a");
     $.ajax({
         type: 'POST',
-        url: 'http://thmsa.azurewebsites.net:1337/LEDon'
+        url: 'http://thmsa.azurewebsites.net/LEDon'
     });
 });
-function getTweets() {
-    console.log("getTweets");
-    $.ajax({
-        type: 'POST',
-        url: 'http://thmsa.azurewebsites.net:1337/LEDon'
-    });
-}
--- public/ts/getTweets.ts
+++ public/ts/getTweets.ts
@@ -2,14 +2,7 @@ $('#ledon-button').click(function() {
     alert("a");
     $.ajax({
         type: 'POST',
-        url: 'http://thmsa.azurewebsites.net:1337/LEDon'
+        url: 'http://thmsa.azurewebsites.net/LEDon'
     });
 });
 
-function getTweets() {
-    console.log("getTweets");
-    $.ajax({
-        type: 'POST',
-        url: 'http://thmsa.azurewebsites.net:1337/LEDon'
-    });
-}
